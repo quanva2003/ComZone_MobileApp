a21112d0224392826e8f1897a24a388fec3270c3
wuavann5076 <qsao2212@gmail.com>
2025-01-12 01:52:31 +0700

    last commit  [QuanVA]

--- src/components/auction/AuctionPulisher.js
+++ src/components/auction/AuctionPulisher.js
@@ -8,13 +8,28 @@ const AuctionPublisher = ({ comic }) => {
     <ScrollView
       style={tw`w-full bg-white p-4 rounded-md border border-gray-300`}
     >
-      <Text style={tw`text-sm pb-2`}>Thông tin chi tiết</Text>
+      <Text style={[tw`text-sm pb-2`, { fontFamily: "REM_bold" }]}>
+        Thông tin chi tiết
+      </Text>
 
       <View style={tw`w-full flex-row items-center py-2 border-b`}>
-        <Text style={tw`w-1/2 text-gray-600 text-xs sm:text-sm`}>Thể loại</Text>
-        <View style={tw`w-1/2 flex-wrap flex-row`}>
+        <Text
+          style={[
+            tw`w-1/2 text-gray-600 text-xs sm:text-sm`,
+            { fontFamily: "REM" },
+          ]}
+        >
+          Thể loại
+        </Text>
+        <View style={tw`w-1/2 flex-wrap flex-row justify-end`}>
           {comic.genres?.map((genre, index) => (
-            <Text key={index} style={tw`text-sky-800`}>
+            <Text
+              key={index}
+              style={[
+                tw`text-sky-800`,
+                { fontFamily: "REM_bold", textAlign: "right" },
+              ]}
+            >
               {genre.name}
               {index < comic.genres.length - 1 && ", "}
             </Text>
@@ -23,79 +38,154 @@ const AuctionPublisher = ({ comic }) => {
       </View>
 
       <View style={tw`w-full flex-row items-center py-2 border-b`}>
-        <Text style={tw`w-1/2 text-gray-600 text-xs sm:text-sm`}>Tác giả</Text>
-        <Text style={tw`w-1/2`}>{comic.author}</Text>
+        <Text
+          style={[
+            tw`w-1/2 text-gray-600 text-xs sm:text-sm`,
+            { fontFamily: "REM" },
+          ]}
+        >
+          Tác giả
+        </Text>
+        <Text
+          style={[tw`w-1/2`, { fontFamily: "REM_bold", textAlign: "right" }]}
+        >
+          {comic.author}
+        </Text>
       </View>
 
       <View style={tw`w-full flex-row items-center py-2 border-b`}>
-        <Text style={tw`w-1/2 text-gray-600 text-xs sm:text-sm`}>
+        <Text
+          style={[
+            tw`w-1/2 text-gray-600 text-xs sm:text-sm`,
+            { fontFamily: "REM" },
+          ]}
+        >
           Phiên bản truyện
         </Text>
-        <Text style={tw`w-1/2`}>{comic.edition.name}</Text>
+        <Text
+          style={[tw`w-1/2`, { fontFamily: "REM_bold", textAlign: "right" }]}
+        >
+          {comic.edition.name}
+        </Text>
       </View>
 
       <View style={tw`flex-row items-center py-2 border-b`}>
-        <Text style={tw`w-1/2 text-gray-600 text-xs sm:text-sm`}>
+        <Text
+          style={[
+            tw`w-1/2 text-gray-600 text-xs sm:text-sm`,
+            { fontFamily: "REM" },
+          ]}
+        >
           Tình trạng
         </Text>
-        <Text style={tw`w-1/2`}>
+        <Text
+          style={[tw`w-1/2`, { fontFamily: "REM_bold", textAlign: "right" }]}
+        >
           {comic.condition.name} ({comic.condition.value}/10)
         </Text>
       </View>
 
       <View style={tw`flex-row items-center py-2 border-b`}>
-        <Text style={tw`w-1/2 text-gray-600 text-xs sm:text-sm`}>
+        <Text
+          style={[
+            tw`w-1/2 text-gray-600 text-xs sm:text-sm`,
+            { fontFamily: "REM" },
+          ]}
+        >
           Mô tả tình trạng
         </Text>
-        <Text style={tw`w-1/2`}>{comic.condition.description}</Text>
+        <Text
+          style={[tw`w-1/2`, { fontFamily: "REM_bold", textAlign: "right" }]}
+        >
+          {comic.condition.description}
+        </Text>
       </View>
 
       {comic?.page && (
         <View style={tw`flex-row items-center py-2 border-b`}>
-          <Text style={tw`w-1/2 text-gray-600 text-xs sm:text-sm`}>
+          <Text
+            style={[
+              tw`w-1/2 text-gray-600 text-xs sm:text-sm`,
+              { fontFamily: "REM" },
+            ]}
+          >
             Số trang
           </Text>
-          <Text style={tw`w-1/2`}>{comic.page}</Text>
+          <Text
+            style={[tw`w-1/2`, { fontFamily: "REM_bold", textAlign: "right" }]}
+          >
+            {comic.page}
+          </Text>
         </View>
       )}
 
       {comic?.publicationYear && (
         <View style={tw`flex-row items-center py-2 border-b`}>
-          <Text style={tw`w-1/2 text-gray-600 text-xs sm:text-sm`}>
+          <Text
+            style={[
+              tw`w-1/2 text-gray-600 text-xs sm:text-sm`,
+              { fontFamily: "REM" },
+            ]}
+          >
             Năm phát hành
           </Text>
-          <Text style={tw`w-1/2`}>{comic.publicationYear}</Text>
+          <Text
+            style={[tw`w-1/2`, { fontFamily: "REM_bold", textAlign: "right" }]}
+          >
+            {comic.publicationYear}
+          </Text>
         </View>
       )}
 
       <View style={tw`flex-row items-center py-2 border-b`}>
-        <Text style={tw`w-1/2 text-gray-600 text-xs sm:text-sm`}>
+        <Text
+          style={[
+            tw`w-1/2 text-gray-600 text-xs sm:text-sm`,
+            { fontFamily: "REM" },
+          ]}
+        >
           Truyện lẻ / Bộ truyện
         </Text>
-        <Text style={tw`w-1/2`}>
+        <Text
+          style={[tw`w-1/2`, { fontFamily: "REM_bold", textAlign: "right" }]}
+        >
           {comic?.quantity === 1 ? "Truyện lẻ" : "Bộ truyện"}
         </Text>
       </View>
 
       {comic?.quantity > 1 && (
         <View style={tw`flex-row items-center py-2 border-b`}>
-          <Text style={tw`w-1/2 text-gray-600 text-xs sm:text-sm`}>
+          <Text
+            style={[
+              tw`w-1/2 text-gray-600 text-xs sm:text-sm`,
+              { fontFamily: "REM" },
+            ]}
+          >
             Số lượng cuốn
           </Text>
-          <Text style={tw`w-1/2`}>{comic?.quantity}</Text>
+          <Text
+            style={[tw`w-1/2`, { fontFamily: "REM_bold", textAlign: "right" }]}
+          >
+            {comic?.quantity}
+          </Text>
         </View>
       )}
 
       {comic?.quantity > 1 && comic.episodesList && (
         <View style={tw`flex-row items-center py-2 border-b`}>
-          <Text style={tw`w-1/2 text-gray-600 text-xs sm:text-sm`}>
+          <Text
+            style={[
+              tw`w-1/2 text-gray-600 text-xs sm:text-sm`,
+              { fontFamily: "REM" },
+            ]}
+          >
             Tên tập, số tập:
           </Text>
-          <View style={tw`w-1/2 flex-row flex-wrap gap-2`}>
+          <View style={tw`w-1/2 flex-row flex-wrap justify-end gap-2`}>
             {comic.episodesList.map((eps, index) => (
               <Text
                 key={index}
-                style={tw`px-2 py-1 rounded-md border border-gray-300`}
+                style={tw`px-2 py-1 rounded-md border border-gray-300 text-right`}
               >
                 {/^[0-9]*$/.test(eps) && "Tập "}
                 {eps}
@@ -107,14 +197,19 @@ const AuctionPublisher = ({ comic }) => {
 
       {comic.merchandises.length > 0 && (
         <View style={tw`flex-row items-center py-2 border-b`}>
-          <Text style={tw`w-1/2 text-gray-600 text-xs sm:text-sm`}>
+          <Text
+            style={[
+              tw`w-1/2 text-gray-600 text-xs sm:text-sm`,
+              { fontFamily: "REM" },
+            ]}
+          >
             Phụ kiện đính kèm
           </Text>
-          <View style={tw`w-1/2 flex-row flex-wrap gap-2`}>
+          <View style={tw`w-1/2 flex-row flex-wrap justify-end gap-2`}>
             {comic.merchandises.map((mer, index) => (
               <Text
                 key={index}
-                style={tw`px-2 py-1 rounded-md border border-gray-300`}
+                style={tw`px-2 py-1 rounded-md border border-gray-300 text-right`}
               >
                 {mer.name}
               </Text>
@@ -124,8 +219,17 @@ const AuctionPublisher = ({ comic }) => {
       )}
 
       <View style={tw`flex-row items-center py-2 border-b`}>
-        <Text style={tw`w-1/2 text-gray-600 text-xs sm:text-sm`}>Bìa</Text>
-        <Text style={tw`w-1/2`}>
+        <Text
+          style={[
+            tw`w-1/2 text-gray-600 text-xs sm:text-sm`,
+            { fontFamily: "REM" },
+          ]}
+        >
+          Bìa
+        </Text>
+        <Text
+          style={[tw`w-1/2`, { fontFamily: "REM_bold", textAlign: "right" }]}
+        >
           {comic.cover === "SOFT"
             ? "Bìa mềm"
             : comic.cover === "HARD"
@@ -135,27 +239,52 @@ const AuctionPublisher = ({ comic }) => {
       </View>
 
       <View style={tw`flex-row items-center py-2 border-b`}>
-        <Text style={tw`w-1/2 text-gray-600 text-xs sm:text-sm`}>Màu sắc</Text>
-        <Text style={tw`w-1/2`}>
+        <Text
+          style={[
+            tw`w-1/2 text-gray-600 text-xs sm:text-sm`,
+            { fontFamily: "REM" },
+          ]}
+        >
+          Màu sắc
+        </Text>
+        <Text
+          style={[tw`w-1/2`, { fontFamily: "REM_bold", textAlign: "right" }]}
+        >
           {comic.color === "GRAYSCALE" ? "Đen trắng" : "Có màu"}
         </Text>
       </View>
 
       {comic.originCountry && (
         <View style={tw`flex-row items-center py-2 border-b`}>
-          <Text style={tw`w-1/2 text-gray-600 text-xs sm:text-sm`}>
+          <Text
+            style={[
+              tw`w-1/2 text-gray-600 text-xs sm:text-sm`,
+              { fontFamily: "REM" },
+            ]}
+          >
             Xuất xứ
           </Text>
-          <Text style={tw`w-1/2`}>{comic.originCountry}</Text>
+          <Text
+            style={[tw`w-1/2`, { fontFamily: "REM_bold", textAlign: "right" }]}
+          >
+            {comic.originCountry}
+          </Text>
         </View>
       )}
 
       {(comic.length || comic.width || comic.thickness) && (
         <View style={tw`flex-row items-center py-2 border-b`}>
-          <Text style={tw`w-1/2 text-gray-600 text-xs sm:text-sm`}>
+          <Text
+            style={[
+              tw`w-1/2 text-gray-600 text-xs sm:text-sm`,
+              { fontFamily: "REM" },
+            ]}
+          >
             Kích thước
           </Text>
-          <Text style={tw`w-1/2`}>
+          <Text
+            style={[tw`w-1/2`, { fontFamily: "REM_bold", textAlign: "right" }]}
+          >
             {`${comic.length || "-"} x ${comic.width || "-"} x ${
               comic.thickness || "-"
             } (cm)`}
